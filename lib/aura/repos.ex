8d1145f1b3b077ea81e03178d6d99c2863c20651
Cam Cook <cam.cook.codes@gmail.com>
2025-05-22 17:28:17 -0400

    feat(repos): get repo

--- lib/aura/repos.ex
+++ lib/aura/repos.ex
@@ -6,11 +6,17 @@ defmodule Aura.Repos do
 
   def get_all_repos do
     with {:ok, %{body: body}} <- Requester.request(:get, "/repos") do
-      body
-      |> Enum.map(fn
-        repo ->
-          HexRepo.build(repo)
-      end)
+      results =
+        body
+        |> Enum.map(&HexRepo.build/1)
+
+      {:ok, results}
+    end
+  end
+
+  def get_repo(repo_name) when is_bitstring(repo_name) do
+    with {:ok, %{body: body}} <- Requester.request(:get, "/repos/#{repo_name}") do
+      {:ok, HexRepo.build(body)}
     end
   end
 end
--- test/aura/repos_test.exs
+++ test/aura/repos_test.exs
@@ -6,6 +6,12 @@ defmodule Aura.ReposTest do
   doctest Repos
 
   test "get_all_repos/0" do
-    assert [%{name: "hexpm"}] = Repos.get_all_repos()
+    assert {:ok, [%{name: "hexpm"}]} = Repos.get_all_repos()
+  end
+
+  test "get_repo/1" do
+    assert {:ok, [hex]} = Repos.get_all_repos()
+    assert {:ok, returned} = Repos.get_repo(hex.name)
+    assert returned.name == hex.name
   end
 end
