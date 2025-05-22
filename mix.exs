db0c3601e39686475a07ad8f181d2d00227abd72
Cam Cook <cam.cook.codes@gmail.com>
2025-05-22 12:37:50 -0400

    ci: incorporate ci, dependabot

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Aura.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
     ]
   end
 
