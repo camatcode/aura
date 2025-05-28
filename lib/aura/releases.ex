9b83c086a60e74ee877b0293b605819cfea5661d
Cam Cook <cam.cook.codes@gmail.com>
2025-05-27 22:42:54 -0400

    feat: retire / un-retire releases

--- lib/aura/releases.ex
+++ lib/aura/releases.ex
@@ -4,9 +4,39 @@ defmodule Aura.Releases do
   alias Aura.Model.HexRelease
   alias Aura.Requester
 
-  def get_release(package_name, version) do
-    with {:ok, %{body: body}} <- Requester.get("/packages/#{package_name}/releases/#{version}") do
+  def get_release(package_name, version, opts \\ []) do
+    with {:ok, %{body: body}} <- Requester.get("/packages/#{package_name}/releases/#{version}", opts) do
       {:ok, HexRelease.build(body)}
     end
   end
+
+  def retire_release(package_name, version, reason \\ :other, message, opts \\ []) when is_bitstring(message) do
+    reason = validate_reason(reason)
+
+    opts = Keyword.merge([json: %{reason: reason, message: message}], opts)
+
+    with {:ok, _} <- Requester.post("/packages/#{package_name}/releases/#{version}/retire", opts) do
+      :ok
+    end
+  end
+
+  def undo_retire_release(package_name, version, opts \\ []) do
+    with {:ok, _} <- Requester.delete("/packages/#{package_name}/releases/#{version}/retire", opts) do
+      :ok
+    end
+  end
+
+  defp validate_reason(reason) when is_bitstring(reason) do
+    reason
+    |> String.downcase()
+    |> String.to_atom()
+    |> validate_reason()
+  end
+
+  defp validate_reason(reason)
+       when reason == :renamed or reason == :security or reason == :invalid or reason == :deprecated do
+    reason
+  end
+
+  defp validate_reason(_), do: :other
 end
--- test/aura/releases_test.exs
+++ test/aura/releases_test.exs
@@ -17,4 +17,21 @@ defmodule Aura.ReleasesTest do
     assert {:ok, release} = Releases.get_release("ex_ftp", "0.9.2")
     assert release.retirement.message
   end
+
+  test "retire/un-retire release" do
+    mock_repo = TestHelper.get_mock_repo()
+    api_key = TestHelper.get_mock_api_key()
+    Application.put_env(:aura, :api_key, api_key)
+    Application.put_env(:aura, :repo_url, mock_repo)
+    [package] = Enum.take(Packages.list_packages(), 1)
+    version = package.releases |> hd() |> Map.get(:version)
+    reason = :deprecated
+    message = "This package has been deprecated in favor of ecto."
+    assert :ok = Releases.retire_release(package.name, version, reason, message)
+
+    assert :ok = Releases.undo_retire_release(package.name, version)
+
+    Application.delete_env(:aura, :repo_url)
+    Application.delete_env(:aura, :api_key)
+  end
 end
