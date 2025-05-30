851313d3257d6b33cd42f00a3e28ff1f39df3372
Cam Cook <cam.cook.codes@gmail.com>
2025-05-29 21:01:09 -0400

    chore: formatting

--- config/dev.exs
+++ config/dev.exs
@@ -2,4 +2,4 @@ import Config
 
 # you need to docker-compose up a test instance of hexpm - see README
 config :aura,
-       repo_url: System.get_env("AURA_REPO_URL", "http://localhost:4000/api")
+  repo_url: System.get_env("AURA_REPO_URL", "http://localhost:4000/api")
