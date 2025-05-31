f995d9081ae25394affd56303d4f675f83d254ae
Cam Cook <cam.cook.codes@gmail.com>
2025-05-31 00:59:24 -0400

    docs: first pass: HexPackage

--- lib/aura/model/hex_package.ex
+++ lib/aura/model/hex_package.ex
@@ -1,15 +1,74 @@
 # SPDX-License-Identifier: Apache-2.0
 defmodule Aura.Model.HexPackage do
-  @moduledoc false
+  @moduledoc """
+  A struct describing a package from a Hex-compliant API
+  """
 
   import Aura.Model.Common
 
+  alias Aura.Model.Common
   alias Aura.Model.HexPackage
   alias Aura.Model.HexPackageDownloadStats
   alias Aura.Model.HexPackageMeta
   alias Aura.Model.HexRelease
 
-  HexPackageDownloadStats
+  @typedoc """
+  Name of the package (e.g `"plug"`)
+  """
+  @type package_name :: String.t()
+
+  @typedoc """
+  The repository the package belongs to (e.g `"hexpm"`)
+  """
+  @type repository :: String.t()
+
+  @typedoc """
+  Whether this package is publicly available
+  """
+  @type private? :: boolean
+
+  @typedoc """
+  Human-readable page for package information
+  """
+  @type html_url :: URI.t()
+
+  @typedoc """
+  Human-readable page for package documentation
+  """
+  @type docs_html_url :: URI.t()
+
+  @typedoc """
+  Type describing a package from a Hex-compliant API
+
+  <!-- tabs-open -->
+  ### 🏷️ Keys
+    * **name** :: `t:package_name/0`
+    * **repository** :: `t:repository/0`
+    * **private** :: `t:private?/0`
+    * **meta** :: `t:map/0`
+    * **downloads** :: `t:map/0`
+    * **releases** :: `t:map/0`
+    * **inserted_at** :: `t:Aura.Model.Common.inserted_at/0`
+    * **updated_at** :: `t:Aura.Model.Common.updated_at/0`
+    * **url** :: `t:Aura.Model.Common.url/0`
+    * **html_url** :: `t:html_url/0`
+    * **docs_html_url** :: `t:docs_html_url/0`
+
+  <!-- tabs-close -->
+  """
+  @type t() :: %HexPackage{
+          name: package_name(),
+          repository: repository(),
+          private: private?(),
+          meta: map(),
+          downloads: map(),
+          releases: map(),
+          inserted_at: Common.inserted_at(),
+          updated_at: Common.updated_at(),
+          url: Common.url(),
+          html_url: html_url(),
+          docs_html_url: docs_html_url()
+        }
 
   defstruct [
     :name,
@@ -25,6 +84,10 @@ defmodule Aura.Model.HexPackage do
     :docs_html_url
   ]
 
+  @doc """
+  Builds a `HexPackage` from a map
+  """
+  @spec build(m :: map) :: HexPackage.t()
   def build(m) when is_map(m) do
     m
     |> prepare()
@@ -35,8 +98,6 @@ defmodule Aura.Model.HexPackage do
   defp serialize(_k, nil), do: nil
   defp serialize(:meta, v), do: HexPackageMeta.build(v)
   defp serialize(:downloads, v), do: HexPackageDownloadStats.build(v)
-
   defp serialize(:releases, v), do: Enum.map(v, &HexRelease.build/1)
-
   defp serialize(_k, v), do: v
 end
