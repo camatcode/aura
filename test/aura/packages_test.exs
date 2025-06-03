8ef089d554dc3a1d04689286c4258b36de99c6d7
Cam Cook <cam.cook.codes@gmail.com>
2025-06-03 12:39:32 -0400

    docs: Aura.Model.HexPackageMeta

--- test/aura/packages_test.exs
+++ test/aura/packages_test.exs
@@ -12,13 +12,13 @@ defmodule Aura.PackagesTest do
     TestHelper.setup_state()
   end
 
-  test "connection-to-hex" do
-    Application.delete_env(:aura, :repo_url)
-    Application.delete_env(:aura, :api_key)
-    "decimal" |> Packages.get_package() |> IO.inspect()
-    # "aura" |> Packages.list_package_owners() |> IO.inspect()
-    #    "aura" |> Aura.Releases.get_release("0.9.0") |> IO.inspect()
-  end
+  #  test "connection-to-hex" do
+  #    Application.delete_env(:aura, :repo_url)
+  #    Application.delete_env(:aura, :api_key)
+  #    "decimal" |> Packages.get_package() |> IO.inspect()
+  #    # "aura" |> Packages.list_package_owners() |> IO.inspect()
+  #    #    "aura" |> Aura.Releases.get_release("0.9.0") |> IO.inspect()
+  #  end
 
   test "package search", _state do
     [search: "ups", sort: :recent_downloads]
