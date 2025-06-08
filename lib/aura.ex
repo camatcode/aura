d87308945117dfdf7ba2e0c80afb7bed0f88fff1
Cam Cook <cam.cook.codes@gmail.com>
2025-06-08 10:56:23 -0400

    docs: add README to landing

--- lib/aura.ex
+++ lib/aura.ex
@@ -1,19 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 defmodule Aura do
-  @moduledoc """
-  Documentation for `Aura`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aura.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc Aura.Doc.readme()
 end
