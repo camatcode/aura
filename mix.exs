01013af69ba87c5b89947dc2e3a3467c6de37321
Cam Cook <cam.cook.codes@gmail.com>
2026-03-15 06:08:00 -0400

    build: v1.0.3

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Aura.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/camatcode/aura"
-  @version "1.0.2"
+  @version "1.0.3"
 
   def project do
     [
@@ -104,14 +104,14 @@ defmodule Aura.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.37", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_license, "~> 0.1.0", only: [:dev, :test], runtime: false},
-      {:styler, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:styler, "~> 1.11", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: [:test]},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_machina, "~> 2.8.0", only: :test},
-      {:faker, "~> 0.18.0", only: :test},
+      {:faker, "~> 0.18", only: :test},
       {:junit_formatter, "~> 3.1", only: [:test]},
       {:req, "~> 0.5"},
       {:proper_case, "~> 1.3"},
